1fcc2bd44b1c3c8bbdc39a465724e362ece874e4
UrfanKarimli <urfankarim@gmail.com>
2023-07-28 23:16:14 +0400

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+{
+    let a = 5;
+    let b = 7;
+
+    a = 7
+    b = 5
+    console.log(a, b);
+}
+
+
+{
+    let x = 5;
+    let y = 7;
+
+    let z = x;
+
+    let result 
+    
+    
+    console.log(x, y);
+
+}
+
+function square () {
+    let a,area,perimeter;
+    a=parseInt(document.getElementById("calc").value);
+    area=a*a;
+    perimeter=4*a;
+    document.getElementById("area").innerHTML="kvadratin sahesi"+area;
+    document.getElementById("perimeter").innerHTML="kvadratin perimetri"+perimeter;
+}
+
+function rectan() {
+    let x,y,area,perimeter;
+    x=parseInt(document.getElementById("length").value);
+    y=parseInt(document.getElementById("height").value);
+    area=x*y;
+    perimeter=2*(x+y)
+    document.getElementById("area").innerHTML="duzbucaqlinin sahesi"+area;
+    document.getElementById("perimeter").innerHTML="duzbucaqlinin perimeteri"+perimeter;
+}
+
+function register () {
+    let c = urfankarim 
+    let d = 4177 
+    if ( c=urfankarim,d=4177 ) 
+    
+    { document.getElementById("submit").innerHTML="background-color:green"}
+}
