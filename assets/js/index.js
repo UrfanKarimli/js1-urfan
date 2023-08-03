345ed9fb87067c46d7c838a80c8fbbbaffdcd15f
UrfanKarimli <urfankarim@gmail.com>
2023-08-03 09:53:08 +0400

    change login function data

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,39 +11,35 @@
 {
     let x = 5;
     let y = 7;
-
-    let z = x;
-
-    let result 
-    
-    
+    let z = 40;
+    z = (x * y) + y;
     console.log(x, y);
 
 }
 
-function square () {
-    let a,area,perimeter;
-    a=parseInt(document.getElementById("calc").value);
-    area=a*a;
-    perimeter=4*a;
-    document.getElementById("area").innerHTML="kvadratin sahesi:"+area;
-    document.getElementById("perimeter").innerHTML="kvadratin perimetri:"+perimeter;
+function square() {
+    let a, area, perimeter;
+    a = parseInt(document.getElementById("calc").value);
+    area = a * a;
+    perimeter = 4 * a;
+    document.getElementById("area").innerHTML = "kvadratin sahesi:" + area;
+    document.getElementById("perimeter").innerHTML = "kvadratin perimetri:" + perimeter;
 }
 
 function rectan() {
-    let x,y,area,perimeter;
-    x=parseInt(document.getElementById("length").value);
-    y=parseInt(document.getElementById("height").value);
-    area=x*y;
-    perimeter=2*(x+y)
-    document.getElementById("arena").innerHTML="duzbucaqlinin sahesi:"+area;
-    document.getElementById("perimetr").innerHTML="duzbucaqlinin perimeteri:"+perimeter;
+    let x, y, area, perimeter;
+    x = parseInt(document.getElementById("length").value);
+    y = parseInt(document.getElementById("height").value);
+    area = x * y;
+    perimeter = 2 * (x + y)
+    document.getElementById("arena").innerHTML = "duzbucaqlinin sahesi:" + area;
+    document.getElementById("perimetr").innerHTML = "duzbucaqlinin perimeteri:" + perimeter;
 }
 
-function register () {
-    let c = urfankarim 
-    let d = 4177 
-    if ( c=urfankarim,d=4177 ) 
-    
-    { document.getElementById("submit").innerHTML="background-color:green"}
+function register(c, d) {
+    let c = urfankarim
+    let d = 4177
+    c === urfankarim && d === 4177 ? alert("success") : alert("something wrong")
+
+    { document.getElementById("submit").innerHTML = "background-color:green" }
 }
